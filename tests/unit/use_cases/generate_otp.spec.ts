393a4897798828d91b7e38979f1fcfc4a38b5239
Caio Vinicius <caiomdev@gmail.com>
2025-07-17 00:43:27 -0300

    test: add unit test for generate top use case

---
+++ tests/unit/use_cases/generate_otp.spec.ts
@@ -0,0 +1,26 @@
+import { test } from '@japa/runner'
+
+import { OTP } from "#entities/OTP"
+import { GenerateOTP } from "#use_cases/GenerateOTP"
+
+class MockOtpRepository {
+  public data: OTP[] = []
+
+  async save(otp: OTP): Promise<OTP> {
+    const currentOtp = new OTP("1", otp.code, otp.email, otp.expiresAt)
+    this.data.push(currentOtp)
+
+    return currentOtp
+  }
+}
+
+test.group('Use cases - Generate OTP', () => {
+  test('Generate an OTP and validate if it has not expired', async ({ assert }) => {
+    const repo = new MockOtpRepository()
+    const useCase = new GenerateOTP(repo)
+    const result = await useCase.execute({ email: "caiomdev@gmail.com" })
+
+    assert.isBelow(result.createdAt, result.expiresAt)
+    assert.isFalse(result.isExpired())
+  })
+})
