130d5409f63913c0a16239e289503ef57ff7d8e4
Caio Vinicius <caiomdev@gmail.com>
2025-07-18 18:04:37 -0300

    feat: add check to verify if a valid otp already exists

--- app/controllers/otps_controller.ts
+++ app/controllers/otps_controller.ts
@@ -1,4 +1,5 @@
 import { PostgresOtpRepository } from '#adapter/repositories/PostgresOtpRepository'
+import { FindOtp } from '#domain/use_cases/FindOtp'
 import { GenerateOTP } from '#domain/use_cases/GenerateOTP'
 import type { HttpContext } from '@adonisjs/core/http'
 import logger from '@adonisjs/core/services/logger'
@@ -6,9 +7,18 @@ import logger from '@adonisjs/core/services/logger'
 export default class OtpController {
   async store({ request, response }: HttpContext) {
     logger.info(`[${request.id()}] Generate OTP`)
+
     const email = request.input('email')
 
     const pgOtpRepo = new PostgresOtpRepository()
+    const findByEmail = new FindOtp(pgOtpRepo)
+
+    const alreadyExist = await findByEmail.execute({ email })
+
+    if (alreadyExist) {
+      return response.status(200).send({ token: alreadyExist.code })
+    }
+
     const generateOtp = new GenerateOTP(pgOtpRepo)
     const otp = await generateOtp.execute({ email })
 
