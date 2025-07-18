c06778cab48cee5680241960bef1c5598b2f7af1
Caio Vinicius <caiomdev@gmail.com>
2025-07-18 18:07:17 -0300

    feat: add an use case to find otp by email

---
+++ app/domain/use_cases/FindOtpByEmail.ts
@@ -0,0 +1,17 @@
+import { OTP } from "#domain/entities/OTP"
+import { OtpRepository } from "#domain/repositories/OtpRepository"
+import { FindOtpDTO } from "#domain/use_cases/FindOtpByEmailDTO"
+
+export class FindOtp {
+  constructor(readonly otpRepository: OtpRepository) {}
+
+  async execute(data: FindOtpDTO): Promise<OTP|null> {
+    const otp = await this.otpRepository.findOtpByEmail(data.email)
+
+    if (!otp || otp.isExpired()) {
+      return null
+    }
+
+    return otp
+  }
+}
