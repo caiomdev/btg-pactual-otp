3c921283c1384971c11a1229b1e101289abcbb7c
Caio Vinicius <caiomdev@gmail.com>
2025-07-19 02:58:03 -0300

    feat: add methods to find an otp by email and id and to define an otp as used

--- app/domain/repositories/OtpRepository.ts
+++ app/domain/repositories/OtpRepository.ts
@@ -3,4 +3,6 @@ import { OTP } from "#domain/entities/OTP";
 export interface OtpRepository {
   save(otp: OTP): Promise<OTP>
   findOtpByEmail(email: string): Promise<OTP|null>
+  findOtpByEmailAndCode(email: string, code: string): Promise<OTP|null>
+  markAsUsed(code: string): Promise<void>
 }
