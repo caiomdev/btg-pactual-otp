74d16fc9432bc1f2d350302e933b8bbdfc53e530
Caio Vinicius <caiomdev@gmail.com>
2025-07-19 01:13:16 -0300

    test: add unit test of the findOtp use case

--- tests/unit/use_cases/generate_otp.spec.ts
+++ tests/unit/use_cases/generate_otp.spec.ts
@@ -2,6 +2,7 @@ import { test } from '@japa/runner'
 
 import { OTP } from "#domain/entities/OTP"
 import { GenerateOTP } from "#domain/use_cases/GenerateOTP"
+import { FindOtpByEmail } from "#domain/use_cases/FindOtpByEmail"
 
 class MockOtpRepository {
   public data: OTP[] = []
@@ -12,9 +13,19 @@ class MockOtpRepository {
 
     return currentOtp
   }
+
+  async findOtpByEmail(email: string): Promise<OTP|null> {
+    const otp = this.data.filter((item) => item.email === email)
+
+    if (otp.length > 0 && otp[0].isExpired()) {
+      return null
+    }
+
+    return otp[0]
+  }
 }
 
-test.group('Use cases - Generate OTP', () => {
+test.group('Use cases', () => {
   test('Generate an OTP and validate if it has not expired', async ({ assert }) => {
     const repo = new MockOtpRepository()
     const useCase = new GenerateOTP(repo)
@@ -22,5 +33,20 @@ test.group('Use cases - Generate OTP', () => {
 
     assert.isBelow(result.createdAt, result.expiresAt)
     assert.isFalse(result.isExpired())
+  })
+
+  test('should find an OTP by email', async ({ assert }) => {
+    const repo = new MockOtpRepository()
+
+    const useCaseGenerateOtp = new GenerateOTP(repo)
+    const otp = await useCaseGenerateOtp.execute({ email: "caiomdev@gmail.com" })
+
+    const useCaseFindOtp = new FindOtpByEmail(repo)
+    const nullableResult = await useCaseFindOtp.execute({ email: "caio@gmail.com" })
+    const validResult = await useCaseFindOtp.execute({ email: "caiomdev@gmail.com" })
+
+    assert.isNull(nullableResult)
+    assert.isNotNull(validResult)
+    assert.strictEqual(otp.code, validResult?.code)
   })
 })
