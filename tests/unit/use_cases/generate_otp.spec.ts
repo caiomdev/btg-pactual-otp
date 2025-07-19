d8d8a52e723df00b8304ec57febd80b1fca28d9e
Caio Vinicius <caiomdev@gmail.com>
2025-07-19 02:58:42 -0300

    feat: add test for validate otp use case

--- tests/unit/use_cases/generate_otp.spec.ts
+++ tests/unit/use_cases/generate_otp.spec.ts
@@ -3,6 +3,7 @@ import { test } from '@japa/runner'
 import { OTP } from "#domain/entities/OTP"
 import { GenerateOTP } from "#domain/use_cases/GenerateOTP"
 import { FindOtpByEmail } from "#domain/use_cases/FindOtpByEmail"
+import { ValidateOtp } from '#domain/use_cases/ValidateOtp'
 
 class MockOtpRepository {
   public data: OTP[] = []
@@ -22,6 +23,24 @@ class MockOtpRepository {
     }
 
     return otp[0]
+  }
+
+  async findOtpByEmailAndCode(email: string, code: string): Promise<OTP|null> {
+    const otp = this.data.filter((item) => item.email === email && item.code === code)
+
+    if (otp.length > 0 && otp[0].isExpired()) {
+      return null
+    }
+
+    return otp[0]
+  }
+
+  async markAsUsed(code: string): Promise<void> {
+    const otp = this.data.filter((item) => item.code === code)
+
+    if (otp.length > 0) {
+      otp[0].setUsedAt(new Date())
+    }
   }
 }
 
@@ -49,5 +68,26 @@ test.group('Use cases', () => {
     assert.isNull(nullableResult)
     assert.isNotNull(validResult)
     assert.strictEqual(otp.code, validResult?.code)
+  })
+
+  test('should validate a valid OTP and invalidate it', async ({ assert }) => {
+    const repo = new MockOtpRepository()
+
+    const createdAt = new Date()
+    const expiresAt = new Date(createdAt.getTime() + 60 * 1000)
+    const otp = new OTP(1, 'A1B2C3', 'caiomdev@gmail.com', expiresAt, createdAt)
+    await repo.save(otp)
+
+    const useCaseValidateOtp = new ValidateOtp(repo)
+    const firstTimeOtpValidation = await useCaseValidateOtp.execute({ email: "caiomdev@gmail.com", code: 'A1B2C3'})
+
+    assert.isUndefined(firstTimeOtpValidation)
+
+    try {
+      await useCaseValidateOtp.execute({ email: "caiomdev@gmail.com", code: 'A1B2C3'})
+      assert.fail('Expected exception')
+    } catch(error) {
+      assert.equal(error.message, 'Token has expired')
+    }
   })
 })
