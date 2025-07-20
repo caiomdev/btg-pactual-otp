91a4516ff590fa14fc36a58b1a27fb2a4375ff5e
Caio Vinicius <caiomdev@gmail.com>
2025-07-20 16:09:24 -0300

    test: add logger to tests

--- tests/unit/use_cases/generate_otp.spec.ts
+++ tests/unit/use_cases/generate_otp.spec.ts
@@ -5,6 +5,16 @@ import { GenerateOTP } from "#application/use_cases/generate_otp"
 import { FindOtpByEmail } from "#application/use_cases/find_otp_by_email"
 import { ValidateOtp } from '#application/use_cases/validate_otp'
 
+class Logger {
+  info(message: string) {
+    console.log(`[INFO] ${message}`)
+  }
+
+  error(message: string){
+    console.log(`[ERROR] ${message}`)
+  }
+}
+
 class MockOtpRepository {
   public data: OTP[] = []
 
@@ -47,7 +57,8 @@ class MockOtpRepository {
 test.group('Use cases', () => {
   test('should generate an OTP and validate if it has not expired', async ({ assert }) => {
     const repo = new MockOtpRepository()
-    const useCase = new GenerateOTP(repo)
+    const logger = new Logger()
+    const useCase = new GenerateOTP(repo, logger)
     const result = await useCase.execute({ email: "caiomdev@gmail.com" })
 
     assert.isBelow(result.createdAt, result.expiresAt)
@@ -57,11 +68,11 @@ test.group('Use cases', () => {
 
   test('should find an OTP by email', async ({ assert }) => {
     const repo = new MockOtpRepository()
-
-    const useCaseGenerateOtp = new GenerateOTP(repo)
+    const logger = new Logger()
+    const useCaseGenerateOtp = new GenerateOTP(repo, logger)
     const otp = await useCaseGenerateOtp.execute({ email: "caiomdev@gmail.com" })
 
-    const useCaseFindOtp = new FindOtpByEmail(repo)
+    const useCaseFindOtp = new FindOtpByEmail(repo, logger)
     const nullableResult = await useCaseFindOtp.execute({ email: "caio@gmail.com" })
     const validResult = await useCaseFindOtp.execute({ email: "caiomdev@gmail.com" })
 
@@ -72,13 +83,13 @@ test.group('Use cases', () => {
 
   test('should validate a valid OTP and invalidate it', async ({ assert }) => {
     const repo = new MockOtpRepository()
-
+    const logger = new Logger()
     const createdAt = new Date()
     const expiresAt = new Date(createdAt.getTime() + 60 * 1000)
     const otp = new OTP(1, 'A1B2C3', 'caiomdev@gmail.com', expiresAt, createdAt)
     await repo.save(otp)
 
-    const useCaseValidateOtp = new ValidateOtp(repo)
+    const useCaseValidateOtp = new ValidateOtp(repo, logger)
     const firstTimeOtpValidation = await useCaseValidateOtp.execute({ email: "caiomdev@gmail.com", code: 'A1B2C3'})
 
     assert.isUndefined(firstTimeOtpValidation)
